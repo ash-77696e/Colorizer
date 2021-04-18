cc0243bb1611af4ad5c260af29c19979b9a81f6b
ash-77696e <haridas.ashwin@gmail.com>
2021-04-18 17:06:09 -0400

    convert image to color/bw matrix

---
+++ Colorizer.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    # STORED AS BGR NOT RGB
+    original_img = cv2.imread('image.png')
+    original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+    #print(original_img)
+
+    left = original_img[:, 0:int(original_img.shape[1] / 2), :]
+    right = original_img[:, int(original_img.shape[1] / 2):original_img.shape[1], :]
+    
+    left_bw = convert_to_grey(left)
+    right_bw = convert_to_grey(right)
+    
+    combined_img = []
+
+    for i in range(0, len(left)):
+        combined_img.append(list(left_bw[i]) + list(right[i]))
+    
+    plt.imshow(combined_img)
+
+    plt.show()
+
+def convert_to_grey(img):
+    bw = np.copy(img)
+    for i in range(0, len(bw)):
+        for j in range(0, len(bw[i])):
+            bw[i][j] = 0.21 * bw[i][j][0] + 0.72 * bw[i][j][1] + 0.07 * bw[i][j][2]
+    
+    return bw
+
+
+if __name__ == '__main__':
+    main()
